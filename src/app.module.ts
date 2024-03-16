31a48151e336e30fb4ceb5242e4d641897747ab9
tugaserya <artempoznakov990@gmail.com>
2024-03-16 14:29:23 +0300

    add env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {ScoresModule} from "./scores/scores.module";
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ScoresModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
